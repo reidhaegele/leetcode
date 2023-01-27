d3547507a0b6de6c75c91181836ccb85ca384dad
Reid Haegele <rahdb4@umsystem.edu>
2023-01-26 18:58:06 -0600

    217 complete

--- 66-plus-one/plusOne.py
+++ 66-plus-one/plusOne.py
@@ -4,23 +4,23 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         index = len(digits) - 1
-        
-        while(digits[index]+1 == 10):
-                digits[index] = 0
-                index-=1
-                if(index<0): 
-                        digits.insert(0, 1)
-                        return digits
-        #if(index)
+
+        while (digits[index]+1 == 10):
+            digits[index] = 0
+            index -= 1
+            if (index < 0):
+                digits.insert(0, 1)
+                return digits
         digits[index] = digits[index]+1
         return digits
 
+
 if __name__ == "__main__":
-        s = Solution()
-        
-        print(s.plusOne([1, 2, 3]))
-        print(s.plusOne([1, 2, 9]))
-        print(s.plusOne([1, 9]))
-        print(s.plusOne([9, 9, 9]))
-        print(s.plusOne([9]))
-        print(s.plusOne([9, 9, 1, 1, 9, 9, 9, 9, 2, 9, 3, 9, 4, 9]))
+    s = Solution()
+
+    print(s.plusOne([1, 2, 3]))
+    print(s.plusOne([1, 2, 9]))
+    print(s.plusOne([1, 9]))
+    print(s.plusOne([9, 9, 9]))
+    print(s.plusOne([9]))
+    print(s.plusOne([9, 9, 1, 1, 9, 9, 9, 9, 2, 9, 3, 9, 4, 9]))
